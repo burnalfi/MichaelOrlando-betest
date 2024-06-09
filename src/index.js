36d13ad632367fc82009e90012b17fe541f03da0
Muhammad Faisal <muhammad.faisal.it-consultant@prudential.co.id>
2024-06-09 07:38:07 +0700

    Fix

--- src/index.js
+++ src/index.js
@@ -17,18 +17,15 @@ const dbUrl = process.env.dbUrl;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/', (req, res) => {
-	res.send('Hello world!');
-})
 app.use('/account', AccountRoute);
 app.use('/user-info', Middleware.verifyToken, UserInfoRoute);
 app.use('/account-login', Middleware.verifyToken, AccountLoginRoute);
 
-app.get('/ping', (req, res) => {
+app.use('/ping', (req, res) => {
 	res.send('pong!');
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 	console.log(`Connecting to database on port ${dbPort}`);
 
 	mongoose
