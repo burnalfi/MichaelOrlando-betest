aeba13b99cc57d20b3abb2a561c1c4909ff4f334
Muhammad Faisal <muhammad.faisal.it-consultant@prudential.co.id>
2024-06-09 15:48:04 +0700

    Port adjustment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const dbPort = process.env.dbPort;
 const dbName = process.env.dbName;
 const dbUrl = process.env.dbUrl;
